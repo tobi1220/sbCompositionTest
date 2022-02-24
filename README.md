31a2f8a6b41523a3e782577ff87dcbc4d1d97965
tobi <cmd02@t-online.de>
2022-02-24 19:02:37 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+run `npm run installs` first
+
+run `npm run first` and `npm run second` simultaneously
---
+++ second/.storybook/main.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/angular",
+  "core": {
+    "builder": "webpack5"
+  },
+  refs: {
+   'first-storybook': {
+     title: "first",
+     url: "http://localhost:6006"
+   }
+  }
+}
